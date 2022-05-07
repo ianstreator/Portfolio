49c3b9bd6a2cbb166a57fd6281b5d4540408779b
Ian Streator <istreator@yahoo.com>
2022-05-07 01:33:46 -0500

    removed unknown/unused technologies

--- images/export.js
+++ images/export.js
@@ -1,10 +1,10 @@
 import figma from "./figma().svg";
-import firebase from "./firebase().svg";
-import gatsby from "./gatsby().svg";
-import go from "./go().svg";
+// import firebase from "./firebase().svg";
+// import gatsby from "./gatsby().svg";
+// import go from "./go().svg";
 import javascript from "./javascript().svg";
-import typescript from "./typescript().svg";
-import strapi from "./strapi().svg";
+// import typescript from "./typescript().svg";
+// import strapi from "./strapi().svg";
 import sass from "./sass().svg";
 import react from "./react().svg";
 import node from "./node().svg";
@@ -12,12 +12,12 @@ import mongoDB from "./mongoDB().svg";
 import postgres from "./postgres().svg";
 
 import figma1 from "./figma(1).svg";
-import firebase1 from "./firebase(1).svg";
-import gatsby1 from "./gatsby(1).svg";
-import go1 from "./go(1).svg";
+// import firebase1 from "./firebase(1).svg";
+// import gatsby1 from "./gatsby(1).svg";
+// import go1 from "./go(1).svg";
 import javascript1 from "./javascript(1).svg";
-import typescript1 from "./typescript(1).svg";
-import strapi1 from "./strapi(1).svg";
+// import typescript1 from "./typescript(1).svg";
+// import strapi1 from "./strapi(1).svg";
 import sass1 from "./sass(1).svg";
 import react1 from "./react(1).svg";
 import node1 from "./node(1).svg";
@@ -28,24 +28,14 @@ import preview from "./preview.png"
 
 const images = [
   figma,
-  firebase,
-  gatsby,
-  go,
   javascript,
-  typescript,
-  strapi,
   sass,
   react,
   node,
   mongoDB,
   postgres,
   figma1,
-  firebase1,
-  gatsby1,
-  go1,
   javascript1,
-  typescript1,
-  strapi1,
   sass1,
   react1,
   node1,
