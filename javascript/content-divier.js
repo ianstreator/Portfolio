b3809befe0e46530e5e66ee239dd23ababa5d5ea
Ian Streator <istreator@yahoo.com>
2022-06-05 17:04:01 -0500

    organizing javascript into separate files, adding image and WakaTime stats to about section

---
+++ javascript/content-divier.js
@@ -0,0 +1,77 @@
+const divider = document.getElementById("divider");
+
+const clientWidth = window.innerWidth * 2;
+const clientHeight = divider.clientHeight;
+
+const hexWidth = 104;
+const hexHeight = 120;
+
+export function createDivider() {
+  for (let i = 0; i * hexHeight < clientHeight * 1.5; i++) {
+    if (i % 2 !== 0) {
+      console.log("even");
+      const row = document.createElement("div");
+      row.classList.add("shapes", "even");
+      for (let j = 0; j * hexWidth < clientWidth * 1.25; j++) {
+        if (Math.random() < 0.5) {
+          createHexagon2(row);
+          continue;
+        }
+        createHexagon(row);
+      }
+    } else {
+      console.log("odd");
+      const row = document.createElement("div");
+      row.classList.add("shapes", "odd");
+      for (let j = 0; j * hexWidth < clientWidth * 1.25; j++) {
+        if (Math.random() < 0.5) {
+          createHexagon2(row);
+          continue;
+        }
+        createHexagon(row);
+      }
+    }
+  }
+}
+
+function createHexagon2(row) {
+  const hexWrap = document.createElement("div");
+  hexWrap.classList.add("hexagon-wrapper");
+
+  const hexTop = document.createElement("div");
+  hexTop.classList.add("hexagon", "hex-top");
+  hexTop.style.cssText = "border-bottom: 30px solid transparent;";
+
+  const hexMid = document.createElement("div");
+  hexMid.classList.add("hexagon", "hex-mid");
+  hexMid.style.cssText = "background: transparent;";
+
+  const hexBot = document.createElement("div");
+  hexBot.classList.add("hexagon", "hex-bot");
+  hexBot.style.cssText = "border-top: 30px solid transparent;";
+
+  hexWrap.append(hexTop, hexMid, hexBot);
+  row.append(hexWrap);
+  divider.append(row);
+}
+
+function createHexagon(row) {
+  const hexWrap = document.createElement("div");
+  hexWrap.classList.add("hexagon-wrapper");
+
+  const hexTop = document.createElement("div");
+  hexTop.classList.add("hexagon", "hex-top");
+
+  const hexMid = document.createElement("div");
+  hexMid.classList.add("hexagon", "hex-mid");
+  const hexShade = document.createElement("div");
+  hexShade.classList.add("hex-shade");
+
+  const hexBot = document.createElement("div");
+  hexBot.classList.add("hexagon", "hex-bot");
+
+  hexMid.append(hexShade);
+  hexWrap.append(hexTop, hexMid, hexBot);
+  row.append(hexWrap);
+  divider.append(row);
+}
--- javascript/main.js
+++ javascript/main.js
@@ -1,12 +1,14 @@
-import technologies from "./images/export.js";
-const imageLink = technologies.preview;
-// const og_img = document.createElement("meta")
-const previewWebsite = document.getElementById("og_img");
-const head = document.querySelector("head");
-previewWebsite.content = imageLink;
+import technologies from "../images/export.js";
+import { createDivider } from "./content-divier.js";
+createDivider();
+// const imageLink = technologies.preview;
+// // const og_img = document.createElement("meta")
+// const previewWebsite = document.getElementById("og_img");
+// const head = document.querySelector("head");
+// previewWebsite.content = imageLink;
 
-head.append(og_img);
-console.log(imageLink);
+// head.append(og_img);
+// console.log(imageLink);
 
 const mutedSkills = {};
 const coloredSkills = {};
@@ -156,7 +158,6 @@ Object.values(card).forEach((e) => {
 //   const img = document.createElement("img");
 //   img.src = e;
 //   x += canvas.width / 7.5;
-  
 
 //   const skill = new Skill(
 //     img,
@@ -192,78 +193,78 @@ Object.values(card).forEach((e) => {
 // }, 25);
 
 //.....divider shape fill....
-const divider = document.getElementById("divider");
+// const divider = document.getElementById("divider");
 
-const clientWidth = window.innerWidth * 2;
-const clientHeight = divider.clientHeight;
+// const clientWidth = window.innerWidth * 2;
+// const clientHeight = divider.clientHeight;
 
-const hexWidth = 104;
-const hexHeight = 120;
+// const hexWidth = 104;
+// const hexHeight = 120;
 
-for (let i = 0; i * hexHeight < clientHeight * 1.5; i++) {
-  if (i % 2 !== 0) {
-    console.log("even");
-    const row = document.createElement("div");
-    row.classList.add("shapes", "even");
-    for (let j = 0; j * hexWidth < clientWidth * 1.25; j++) {
-      if (Math.random() < 0.5) {
-        createHexagon2(row);
-        continue;
-      }
-      createHexagon(row);
-    }
-  } else {
-    console.log("odd");
-    const row = document.createElement("div");
-    row.classList.add("shapes", "odd");
-    for (let j = 0; j * hexWidth < clientWidth * 1.25; j++) {
-      if (Math.random() < 0.5) {
-        createHexagon2(row);
-        continue;
-      }
-      createHexagon(row);
-    }
-  }
-}
+// for (let i = 0; i * hexHeight < clientHeight * 1.5; i++) {
+//   if (i % 2 !== 0) {
+//     console.log("even");
+//     const row = document.createElement("div");
+//     row.classList.add("shapes", "even");
+//     for (let j = 0; j * hexWidth < clientWidth * 1.25; j++) {
+//       if (Math.random() < 0.5) {
+//         createHexagon2(row);
+//         continue;
+//       }
+//       createHexagon(row);
+//     }
+//   } else {
+//     console.log("odd");
+//     const row = document.createElement("div");
+//     row.classList.add("shapes", "odd");
+//     for (let j = 0; j * hexWidth < clientWidth * 1.25; j++) {
+//       if (Math.random() < 0.5) {
+//         createHexagon2(row);
+//         continue;
+//       }
+//       createHexagon(row);
+//     }
+//   }
+// }
 
-function createHexagon2(row) {
-  const hexWrap = document.createElement("div");
-  hexWrap.classList.add("hexagon-wrapper");
+// function createHexagon2(row) {
+//   const hexWrap = document.createElement("div");
+//   hexWrap.classList.add("hexagon-wrapper");
 
-  const hexTop = document.createElement("div");
-  hexTop.classList.add("hexagon", "hex-top");
-  hexTop.style.cssText = "border-bottom: 30px solid transparent;";
+//   const hexTop = document.createElement("div");
+//   hexTop.classList.add("hexagon", "hex-top");
+//   hexTop.style.cssText = "border-bottom: 30px solid transparent;";
 
-  const hexMid = document.createElement("div");
-  hexMid.classList.add("hexagon", "hex-mid");
-  hexMid.style.cssText = "background: transparent;";
+//   const hexMid = document.createElement("div");
+//   hexMid.classList.add("hexagon", "hex-mid");
+//   hexMid.style.cssText = "background: transparent;";
 
-  const hexBot = document.createElement("div");
-  hexBot.classList.add("hexagon", "hex-bot");
-  hexBot.style.cssText = "border-top: 30px solid transparent;";
+//   const hexBot = document.createElement("div");
+//   hexBot.classList.add("hexagon", "hex-bot");
+//   hexBot.style.cssText = "border-top: 30px solid transparent;";
 
-  hexWrap.append(hexTop, hexMid, hexBot);
-  row.append(hexWrap);
-  divider.append(row);
-}
+//   hexWrap.append(hexTop, hexMid, hexBot);
+//   row.append(hexWrap);
+//   divider.append(row);
+// }
 
-function createHexagon(row) {
-  const hexWrap = document.createElement("div");
-  hexWrap.classList.add("hexagon-wrapper");
+// function createHexagon(row) {
+//   const hexWrap = document.createElement("div");
+//   hexWrap.classList.add("hexagon-wrapper");
 
-  const hexTop = document.createElement("div");
-  hexTop.classList.add("hexagon", "hex-top");
+//   const hexTop = document.createElement("div");
+//   hexTop.classList.add("hexagon", "hex-top");
 
-  const hexMid = document.createElement("div");
-  hexMid.classList.add("hexagon", "hex-mid");
-  const hexShade = document.createElement("div");
-  hexShade.classList.add("hex-shade");
+//   const hexMid = document.createElement("div");
+//   hexMid.classList.add("hexagon", "hex-mid");
+//   const hexShade = document.createElement("div");
+//   hexShade.classList.add("hex-shade");
 
-  const hexBot = document.createElement("div");
-  hexBot.classList.add("hexagon", "hex-bot");
+//   const hexBot = document.createElement("div");
+//   hexBot.classList.add("hexagon", "hex-bot");
 
-  hexMid.append(hexShade);
-  hexWrap.append(hexTop, hexMid, hexBot);
-  row.append(hexWrap);
-  divider.append(row);
-}
+//   hexMid.append(hexShade);
+//   hexWrap.append(hexTop, hexMid, hexBot);
+//   row.append(hexWrap);
+//   divider.append(row);
+// }
